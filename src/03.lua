c8009c17ded1981d18b742244ab988b4e812bc7d
Sebastian Plaza <sebaplaza@users.noreply.github.com>
2023-12-03 22:45:01 +0100

    feat(03): prepare skeleton

---
+++ src/03.lua
@@ -0,0 +1,9 @@
+local function run(input_file)
+
+  local total = 4361
+
+  print("total", total)
+  return total
+end
+
+return run
---
+++ src/03.spec.lua
@@ -0,0 +1,9 @@
+local run = require("03")
+
+describe("gear ratios", function()
+    it("should sum part numbers (input-example)", function()
+        local result = run("resources/03/input-example.txt")
+        assert.are.same(4361, result)
+    end)
+
+end)
