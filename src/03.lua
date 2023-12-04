474a98dd54a05948e596605fd946bcbc4ec15fd6
Sebastian Plaza <sebaplaza@users.noreply.github.com>
2023-12-04 21:21:45 +0100

    feat(03): wip

--- src/03.lua
+++ src/03.lua
@@ -1,9 +1,36 @@
+local lrex = require("rex_pcre")
+local utils = require("common.utils")
+
+local function parse_line(line)
+    local line_info = {}
+    local patternNumbers = "(\\d+)"
+
+    for match in lrex.gmatch(line, patternNumbers) do
+        print('match', match)
+    end
+
+end
+
+local function generate_sum(input_file)
+
+    local file_content = utils.read_file(input_file)
+    local all_lines = utils.split_by_lines(file_content)
+    local lines_content = {}
+
+    local total = 0
+    for key, line in pairs(all_lines) do
+        local parsed = parse_line(line);
+    end
+
+    return 4361
+
+end
+
 local function run(input_file)
 
-  local total = 4361
-
-  print("total", total)
-  return total
+    local total = generate_sum(input_file)
+    print("total", total)
+    return total
 end
 
 return run
--- src/03.spec.lua
+++ src/03.spec.lua
@@ -1,6 +1,7 @@
 local run = require("03")
 
 describe("gear ratios", function()
+
     it("should sum part numbers (input-example)", function()
         local result = run("resources/03/input-example.txt")
         assert.are.same(4361, result)
